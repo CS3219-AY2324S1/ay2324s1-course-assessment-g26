81be6c8afae42d6f8ba316fd99b7364e09dec965
csiongn <choonsiongn@gmail.com>
2023-11-12 20:14:57 +0800

    Add API in express for managing users

---
+++ backend/user-service/app.js
@@ -0,0 +1,19 @@
+// user-service/backend/app.js
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import authRoute from './routes/auth.js';
+import userRoute from './routes/user.js';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/auth', authRoute);
+app.use('/user', userRoute);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

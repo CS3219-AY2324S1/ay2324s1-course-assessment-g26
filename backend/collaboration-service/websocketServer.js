4413ce369d8a13b7e285dd695bd7a05f41380381
chinkiat <e0544173@u.nus.edu>
2023-11-13 11:12:03 +0800

    remove services for assignment 5 onwards

--- backend/collaboration-service/websocketServer.js
+++
@@ -1,7 +0,0 @@
-const WebSocket = require('ws');
-const { setupWSConnection } = require('y-websocket/bin/utils');
-const wss = new WebSocket.Server({ noServer: true });
-
-wss.on('connection', setupWSConnection);
-
-module.exports = wss;
